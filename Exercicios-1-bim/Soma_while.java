f8079ac6b908275827c71c554791e17c4af04533
Vinicius gomes <niciugcruz@gmail.com>
2024-07-23 20:10:13 -0300

    correção sout

--- Exercicios-1-bim/Soma_while.java
+++ Exercicios-1-bim/Soma_while.java
@@ -4,20 +4,22 @@ public class Soma_while {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
 
-        int a,b,soma = 0;
+        int i,a,b,soma = 0;
 
         System.out.println("Digite o 1 numero");
         a = entrada.nextInt();
         System.out.println("Digite o 2 numero");
         b = entrada.nextInt();
 
-        while (a <= b) {
-            soma += a;
-            a++;
+        i = a;
+
+        while (i <= b) {
+            soma += i;
+            i++;
         }
 
         entrada.close();
 
-        System.out.println("Soma " + soma);
+        System.out.println("A soma dos valores de " + a + " a " +  b + " é " + soma);
     }
 }
