b7d1287dacd18747442fca3eff615b92b6dc82ce
Vinicius gomes <niciugcruz@gmail.com>
2024-07-23 19:48:55 -0300

    soma while

---
+++ Exercicios-1-bim/Soma_while.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Soma_while {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        int a,b,soma;
+
+        soma = 0;
+
+        System.out.println("Digite o 1 numero");
+        a = entrada.nextInt();
+        System.out.println("Digite o 2 numero");
+        b = entrada.nextInt();
+
+        while (a <= b) {
+            soma += a;
+            a++;
+        }
+
+        entrada.close();
+
+        System.out.println("Soma " + soma);
+    }
+}
