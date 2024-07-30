ea7c05a94d96b963dcc38e05cab66b11c4cbb364
vinicius <niciugcruz@gmail.com>
2024-07-30 20:30:14 -0300

    primo

--- Exercicios-1-bim/Primo.java
+++ Exercicios-1-bim/Primo.java
@@ -4,7 +4,6 @@ public class Primo {
     }
 
     public static void primo(int n){
-        int primo = 0;
         for (int i = 2; i < n; i++) {
             if (n % i == 0) {
                 System.out.println("O numero não é primo");
