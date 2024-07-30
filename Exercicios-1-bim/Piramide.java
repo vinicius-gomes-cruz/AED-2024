2420316528eeb6a684e63e6f25748fad4f3ec242
vinicius <niciugcruz@gmail.com>
2024-07-30 19:30:09 -0300

    for piramide

--- Exercicios-1-bim/Piramide.java
+++ Exercicios-1-bim/Piramide.java
@@ -1,6 +1,6 @@
 public class Piramide {
     public static void main(String[] args) {
-        piramide(7);
+        piramidefor(7);
     }
 
     public static void piramide(int n){
@@ -17,6 +17,18 @@ public class Piramide {
                 i++;
             }
             j++;
+        }
+    }
+
+    public static void piramidefor(int n){
+        for (int j = 1; j <= n; j++) {
+            for (int i = 1; i <= j; i++) {
+                if (i == j) {
+                    System.out.print("\n");
+                    continue;
+                }
+                System.out.print(i + " ");
+            }
         }
     }
 }
