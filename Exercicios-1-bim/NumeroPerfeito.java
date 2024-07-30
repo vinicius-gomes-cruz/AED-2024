753c0ea088fa48e2fd467ffe96d9a9a38516d954
vinicius <niciugcruz@gmail.com>
2024-07-30 20:34:57 -0300

    numeros perfeitos ate 10000

--- Exercicios-1-bim/NumeroPerfeito.java
+++ Exercicios-1-bim/NumeroPerfeito.java
@@ -1,19 +1,20 @@
 public class NumeroPerfeito {
     public static void main(String[] args) {
-        numperfeito(28);
+        numperfeito();
     }
 
-    public static void numperfeito(int n){
-        int soma = 0;
-        for (int i = 1; i < n; i++) {
-            if (n % i == 0) {
-                soma += i;
+    public static void numperfeito() {
+        for (int n = 1; n <= 10000; n++) {
+            int soma = 0;
+            for (int i = 1; i < n; i++) {
+                if (n % i == 0) {
+                    soma += i;
+                }
+            }
+            if (soma == n) {
+                System.out.println(n);
             }
         }
-        if (soma == n) {
-            System.out.println("o numero é perfeito");
-        } else {
-            System.out.println("o numero não é perfeito");
-        }
+
     }
 }
