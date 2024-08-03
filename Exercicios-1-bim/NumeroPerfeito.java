3582fa8fe1edfba597424352b06b3c6f12c27fb2
Vinicius <niciugcruz@gmail.com>
2024-08-02 21:17:08 -0300

    boolean numperfeito

--- Exercicios-1-bim/NumeroPerfeito.java
+++ Exercicios-1-bim/NumeroPerfeito.java
@@ -1,11 +1,13 @@
 public class NumeroPerfeito {
     public static void main(String[] args) {
         for (int n = 1; n <= 10000; n++) {
-            numperfeito(n);
+            if (numperfeito(n)) {
+                System.out.println(n);
+            }
         }
     }
 
-    public static void numperfeito(int n) {
+    public static boolean numperfeito(int n) {
         int soma = 0;
         for (int i = 1; i < n; i++) {
             if (n % i == 0) {
@@ -13,7 +15,9 @@ public class NumeroPerfeito {
             }
         }
         if (soma == n) {
-            System.out.println(n);
+            return true;
+        } else {
+            return false;
         }
 
     }
