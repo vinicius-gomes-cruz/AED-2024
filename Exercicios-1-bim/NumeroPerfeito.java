bf97999063a33c087e0019f2ac8789b246aeabfe
Vinicius <niciugcruz@gmail.com>
2024-08-02 21:14:42 -0300

    função numperfeito

--- Exercicios-1-bim/NumeroPerfeito.java
+++ Exercicios-1-bim/NumeroPerfeito.java
@@ -1,19 +1,19 @@
 public class NumeroPerfeito {
     public static void main(String[] args) {
-        numperfeito();
+        for (int n = 1; n <= 10000; n++) {
+            numperfeito(n);
+        }
     }
 
-    public static void numperfeito() {
-        for (int n = 1; n <= 10000; n++) {
-            int soma = 0;
-            for (int i = 1; i < n; i++) {
-                if (n % i == 0) {
-                    soma += i;
-                }
+    public static void numperfeito(int n) {
+        int soma = 0;
+        for (int i = 1; i < n; i++) {
+            if (n % i == 0) {
+                soma += i;
             }
-            if (soma == n) {
-                System.out.println(n);
-            }
+        }
+        if (soma == n) {
+            System.out.println(n);
         }
 
     }
