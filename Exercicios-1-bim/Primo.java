583dad452b37f48135bbd564b55ea3b64d1977b1
Vinicius <niciugcruz@gmail.com>
2024-08-02 21:32:28 -0300

    primo e qtdprimos

--- Exercicios-1-bim/Primo.java
+++ Exercicios-1-bim/Primo.java
@@ -1,13 +1,6 @@
 public class Primo {
     public static void main(String[] args) {
-        int n_primos = 0;
-        int n = 2;
-        while (n_primos < 5) {
-            if (qtdprimo(n)) {
-                n_primos += 1;
-            }
-            n++;
-        }
+        primo(3);
     }
 
     public static void primo(int n){
@@ -19,16 +12,5 @@ public class Primo {
         }
 
         System.out.println("O numero é primo");
-    }
-
-    public static boolean qtdprimo(int n){
-        for (int i = 2; i < n; i++) {
-            if (n % i == 0) {
-                return false;
-            }
-        }
-
-        System.out.println(n);
-        return true;
     }
 }
