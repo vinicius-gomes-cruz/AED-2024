7409ff6f13e7c42367793cdfb1c3676757b541ca
Vinicius <niciugcruz@gmail.com>
2024-08-02 21:34:49 -0300

    a qtdpri

--- Exercicios-1-bim/qtdPrimos.java
+++ Exercicios-1-bim/qtdPrimos.java
@@ -1,7 +1,6 @@
 public class qtdPrimos {
     public static void main(String[] args) {
-        int n_primos = 0;
-        int n = 2;
+        int n_primos = 0, n = 2;
         while (n_primos < 5) {
             if (qtdprimo(n)) {
                 n_primos += 1;
@@ -9,14 +8,12 @@ public class qtdPrimos {
             n++;
         }
     }
-
     public static boolean qtdprimo(int n){
         for (int i = 2; i < n; i++) {
             if (n % i == 0) {
                 return false;
             }
         }
-
         System.out.println(n);
         return true;
     }
