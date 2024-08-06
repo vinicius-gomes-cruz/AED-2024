287af19a18743251521e51efd028a3ae2c90564d
Vinicius <niciugcruz@gmail.com>
2024-08-06 20:39:37 -0300

    hanoi

---
+++ Exercicios-1-bim/Hanoi.java
@@ -0,0 +1,11 @@
+public class Hanoi {
+    public static void main(String[] args) {
+        hanoi(3);
+    }
+
+    public static int hanoi(int discos){
+        if () {
+            
+        }
+    }
+}
