bacc659ed07c5e23ebc37bfefc1d688d5d7ca145
Vinicius Gomes <niciugcruz@gmail.com>
2024-09-30 21:52:34 -0300

    feat: Arquivos

--- aed/aulas/Aula.java
+++ aed/aulas/Aula.java
@@ -2,6 +2,7 @@ package aulas;
 
 import java.util.Arrays;
 
+import libs.Arquivos;
 import libs.Entrada;
 import libs.Exercicios;
 import libs.Matrizes;
@@ -9,18 +10,20 @@ import libs.Vetores;
 
 public class Aula {
     public static void aula(){
-        Entrada.abrir();
-        int[][] mA ={{2,3},
-                    {1,0},
-                    {4,5}};
-        int[][] mB ={{3,1},
-                    {2,4},
-};
-        int[][] resultado = Matrizes.matrizMultInt(mA, mB);
-        for (int i = 0; i < resultado.length; i++) {
-            System.out.println(Arrays.toString(resultado[i]));
-        }
-        // System.out.println(Matrizes.compararMatrizes(mA, mB));
-        Entrada.fechar();
+//         Entrada.abrir();
+//         int[][] mA ={{2,3},
+//                     {1,0},
+//                     {4,5}};
+//         int[][] mB ={{3,1},
+//                     {2,4},
+// };
+//         int[][] resultado = Matrizes.matrizMultInt(mA, mB);
+//         for (int i = 0; i < resultado.length; i++) {
+//             System.out.println(Arrays.toString(resultado[i]));
+//         }
+//         // System.out.println(Matrizes.compararMatrizes(mA, mB));
+//         Entrada.fechar();
+        System.out.println("Aula de Arquivos");
+        Arquivos.ex1();
     }
 }
--- aed/libs/Arquivos.java
+++ aed/libs/Arquivos.java
@@ -1,7 +1,25 @@
 package libs;
 
+import java.io.FileWriter;
+
 public class Arquivos {
+
     public static void ex1() {
+        String nome_arquivo = "teste.txt";
         
+        try{
+            FileWriter arquivo = new FileWriter(nome_arquivo);
+
+            arquivo.write("Algoritmos.\n");
+
+            for (int i = 0; i < 10; i++) {
+                arquivo.write(i + "\n");
+            }
+
+            arquivo.close();
+        } catch (Exception e) {
+            System.out.println("Erro");
+            System.err.println(e);
+        }
     }
 }
