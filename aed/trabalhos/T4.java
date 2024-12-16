b41f19c4db89bef0df65c6effbf331bdaa148ecc
viniciusgomes <124798795+vinicius-gomes-cruz@users.noreply.github.com>
2024-12-16 18:48:12 -0300

    Add files via upload

--- aed/trabalhos/T4.java
+++ aed/trabalhos/T4.java
@@ -25,8 +25,8 @@ public class T4 {
 
             medirETempo("Merge sort", tempos, vetorOriginal, Vetores::mergeSort);
             medirETempo("Quick sort", tempos, vetorOriginal, v -> Vetores.quickSort(v, 0, v.length));
-            medirETempo("Counting sort", tempos, vetorOriginal, T4::countingSort);
-            medirETempo("Radix sort", tempos, vetorOriginal, T4::radixSort);
+            medirETempo("Counting sort", tempos, vetorOriginal, Vetores::countingSort2);
+            medirETempo("Radix sort", tempos, vetorOriginal, Vetores::radixSort);
 
             resultados.put(tamanho, tempos);
             System.out.println();
@@ -48,61 +48,6 @@ public class T4 {
         tempos.put(nome, tempo);
     }
 
-    public static void countingSort(int[] v) {
-        int max = encontrarMax(v);
-        int[] count = new int[max + 1];
-        int[] output = new int[v.length];
-
-        for (int num : v) {
-            count[num]++;
-        }
-
-        for (int i = 1; i < count.length; i++) {
-            count[i] += count[i - 1];
-        }
-
-        for (int i = v.length - 1; i >= 0; i--) {
-            output[--count[v[i]]] = v[i];
-        }
-
-        System.arraycopy(output, 0, v, 0, v.length);
-    }
-
-    public static void radixSort(int[] v) {
-        int max = encontrarMax(v);
-        for (int exp = 1; max / exp > 0; exp *= 10) {
-            countingSortForRadix(v, exp);
-        }
-    }
-
-    private static void countingSortForRadix(int[] v, int exp) {
-        int[] count = new int[10];
-        int[] output = new int[v.length];
-
-        for (int num : v) {
-            count[(num / exp) % 10]++;
-        }
-
-        for (int i = 1; i < count.length; i++) {
-            count[i] += count[i - 1];
-        }
-
-        for (int i = v.length - 1; i >= 0; i--) {
-            output[--count[(v[i] / exp) % 10]] = v[i];
-        }
-
-        System.arraycopy(output, 0, v, 0, v.length);
-    }
-
-    private static int encontrarMax(int[] v) {
-        int max = v[0];
-        for (int num : v) {
-            if (num > max) {
-                max = num;
-            }
-        }
-        return max;
-    }
 
     private static void exibirResultados(int[] tamanhosVetor, Map<Integer, Map<String, Double>> resultados) {
         for (int tamanho : tamanhosVetor) {
